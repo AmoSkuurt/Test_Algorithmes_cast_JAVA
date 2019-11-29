f0fabbdd53ac0a02d39b79db9aa95a80825416b2
AmoSkuurt <amodogbe@gmail.com>
2019-11-29 08:08:01 +0100

    Conditionnelle Exo 1

---
+++ src/com/xix/ayyub/structure_de_controle/Note.java
@@ -0,0 +1,54 @@
+package com.xix.ayyub.structure_de_controle;
+
+import java.util.Scanner;
+
+public class Note {
+    public static void main(String[] args)
+    {
+        /*
+        Algorithme Lire-note-afficher-commentaire-associé
+        variable: note: entier;
+
+        lire(note);
+        si(note <= 8)
+        {
+            ecrire("insuffisant");
+        }
+        sinon si(note <= 12)
+        {
+            ecrire("moyen");
+        }
+        sinon si(note <= 16)
+        {
+            ecrire("bien");
+        }
+        sinon
+        {
+            ecrire("très bien !");
+        }
+        Fin
+         */
+
+        int note;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir votre note: ");
+        note = scanner.nextInt();
+        if (note <= 8)
+        {
+            System.out.println("Insuffisant");
+        }
+        else if (note <= 12)
+        {
+            System.out.println("Moyen");
+        }
+        else if (note <= 16)
+        {
+            System.out.println("Bien");
+        }
+        else
+        {
+            System.out.println("Très bien !");
+        }
+
+    }
+}
---
+++ src/com/xix/ayyub/structure_de_controle/Tarif.java
@@ -0,0 +1,44 @@
+package com.xix.ayyub.structure_de_controle;
+
+import java.util.Scanner;
+
+public class Tarif {
+    public static void main(String[] args)
+    {
+
+
+        String[] tarif = {"Vert", "Orange", "Rouge", "Pas assuré"};
+        int age, nbrAccidents;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir votre âge: ");
+        age = scanner.nextInt();
+        System.out.println("Veuillez saisir le nombre d'accident: ");
+        nbrAccidents = scanner.nextInt();
+        if (age < 25 && nbrAccidents == 0)
+        {
+            System.out.println(tarif[1]);
+        }
+        else if (age >= 25 && nbrAccidents == 0)
+        {
+            System.out.println(tarif[0]);
+        }
+        else if (age < 25 && nbrAccidents <= 2)
+        {
+            System.out.println(tarif[2]);
+        }
+        else if (age >= 25 && nbrAccidents <= 2)
+        {
+            System.out.println(tarif[1]);
+        }
+        else if (age >= 25 && nbrAccidents <= 6)
+        {
+            System.out.println(tarif[2]);
+        }
+        else
+        {
+            System.out.println(tarif[3]);
+        }
+
+    }
+}
